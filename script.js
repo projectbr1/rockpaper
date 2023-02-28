a5e7670aa145e1f48470dfd7582b72d9a94c9599
projectbr1 <125401417+projectbr1@users.noreply.github.com>
2023-02-28 17:14:55 +0100

    introduce score and 5 game rounds functions

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function getComputerChoice()
  return array[Math.floor(Math.random() * 3)]
 }
 
-
+let score1 = 0;
+let score2 = 0;
 const input = window.prompt("rock, paper or scissor")
 let computerSelection = getComputerChoice();
 let playerSelection = input.toLowerCase()
@@ -22,22 +23,29 @@ function playRound(playerSelection, computerSelection)
     return "Both have scissor, remis"
  }else if (playerSelection === "rock" && computerSelection === "scissor")
  {
+    score2 +=1;
     return "You have won! Rock beats scissor"
  }else if (playerSelection === "paper" && computerSelection === "rock")
  {
+    score2 +=1;
     return "You have won! Paper beats Rock"
  }else if (playerSelection === "scissor" && computerSelection === "paper")
  {
+    score2 +=1;
     return "You have won! Scissor beats Paper"
  }else if(playerSelection === "scissor" && computerSelection === "rock")
  {
+    score1 +=1;
     return "The computer has won! Rock beats Scissor"
  }else if(playerSelection === "rock" && computerSelection === "paper")
  {
+    score1 +=1;
     return "The computer has won! Paper beats Rock"
  }else if(playerSelection === "paper" && computerSelection === "scissor")
  {
+    score1 += 1;
     return "The computer has won! Scissor beats Paper"
+    
  }
  
 
@@ -45,14 +53,14 @@ function playRound(playerSelection, computerSelection)
 
 function game()
 {
-    let score1 = 0;
-    let score2 = 0;
+    
     for(let i = 0; i < 5; i++)
     {
         let variable = getComputerChoice()
         console.log(playRound(playerSelection,variable))
-        
-    
     }
 }
+
 game()
+console.log(score1)
+console.log(score2)
